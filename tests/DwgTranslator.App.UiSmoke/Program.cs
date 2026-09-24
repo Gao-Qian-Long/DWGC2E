0320d749aedb13173ef733eba0d44bf6e1dfbf51
GaoQianLong <2528833214@qq.com>
2026-09-24 12:38:35 +0800

    fix(app): route task retargeting through preview input

--- tests/DwgTranslator.App.UiSmoke/Program.cs
+++ tests/DwgTranslator.App.UiSmoke/Program.cs
@@ -462,7 +462,7 @@ public sealed partial class SmokeApp : App
         taskTable.SelectedItem = vm.DrawingFiles[1];
         detailRow.RaiseEvent(new System.Windows.Input.MouseButtonEventArgs(System.Windows.Input.Mouse.PrimaryDevice, Environment.TickCount, System.Windows.Input.MouseButton.Right)
         {
-            RoutedEvent = UIElement.MouseRightButtonDownEvent
+            RoutedEvent = UIElement.PreviewMouseRightButtonDownEvent
         });
         await Dispatcher.InvokeAsync(() => { }, DispatcherPriority.ApplicationIdle);
         Check(vm.SelectedBatchTask == vm.DrawingFiles[0] && taskTable.SelectedItem == vm.DrawingFiles[0],
